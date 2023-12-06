76a67c47727f930d4d62dd5da9c35992b8da0dad
aria-51 <daria9811@gmail.com>
2023-12-06 10:46:07 -0500

    added test bits frontendd

--- test_app.py
+++ test_app.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title('This is a Test')
 st.title("This is not")
+
+
+with st.form("myform"):
+    topic_text = st.text_input("Enter prompt:", "")
+    submitted = st.form_submit_button("Submit")
